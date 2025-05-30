5c82898337ef2a173446ce3cf271f45548830d8d
Ayoni02 <105247084+ayoni02@users.noreply.github.com>
2025-05-30 09:24:25 +0100

    Create Polygons Reverse.py

---
+++ Polygons Reverse.py
@@ -0,0 +1,10 @@
+Polygons_Reverse = {
+    3 : "Triangle",
+    4 : "Square",
+    5 : "Pentagon",
+    6 : "Hexagon",
+    7 : "Heptagon",
+    8 : "Octagon",
+    9 : "Nonagon",
+    10 : "Decagon"
+}
